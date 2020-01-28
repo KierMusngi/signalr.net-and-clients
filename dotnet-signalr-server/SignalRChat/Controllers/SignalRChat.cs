89b5b7eb9b271dcfaabeae2c7dfc2177edf59d9e
Kier Musngi <musngi.kier@gmail.com>
2020-01-29 00:03:48 +0800

    Refactored signalr .net server

---
+++ dotnet-signalr-server/SignalRChat/Controllers/SignalRChat.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SignalRChat : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("SignalR server running.");
+        }
+    }
+}
